dce2324b3724a36dc69930457f9afaaa9d19a29b
夏客 <caic1101@qq.com>
2023-12-28 17:16:44 +0800

    first commit

---
+++ qslib/src/main/java/com/cct/qslib/QslibApplication.java
@@ -0,0 +1,13 @@
+package com.cct.qslib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QslibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QslibApplication.class, args);
+    }
+
+}
---
+++ qslib/src/test/java/com/cct/qslib/QslibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cct.qslib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QslibApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
