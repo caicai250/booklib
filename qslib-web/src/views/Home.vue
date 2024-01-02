712ad86fe938a7f34677bbd8382bc92df278c4b2
caicai250 <1551316691@qq.com>
2024-01-02 09:22:17 +0800

    初始化

--- qslib-web/src/views/Home.vue
+++ qslib-web/src/views/Home.vue
@@ -3,7 +3,20 @@
     <el-container>
       <el-header>Header</el-header>
 
-      <el-main>Main</el-main>
+      <el-main><el-row :gutter="12">
+        <el-col :span="8">
+          <el-card shadow="hover" class="box-card"> <div class="text item">系统架构师</div>
+            <template #footer>题目数量：100  完成率80%</template> </el-card>
+        </el-col>
+        <el-col :span="8">
+          <el-card shadow="hover" class="box-card"> <div class="text item">系统架构师</div>
+            <template #footer>题目数量：100  完成率80%</template> </el-card>
+        </el-col>
+        <el-col :span="8">
+          <el-card shadow="hover" class="box-card"> <div class="text item">系统架构师</div>
+            <template #footer>题目数量：100  完成率80%</template> </el-card>
+        </el-col>
+      </el-row></el-main>
 
       <el-footer>Footer</el-footer>
     </el-container>
@@ -23,5 +36,21 @@ export default defineComponent({
 </script>
 
 <style>
+.card-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 
+.text {
+  font-size: 14px;
+}
+
+.item {
+  margin-bottom: 18px;
+}
+
+.box-card {
+  border-radius: 22px;
+}
 </style>
