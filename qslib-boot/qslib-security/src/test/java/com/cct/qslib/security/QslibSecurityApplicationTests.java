9296a79bd8c967c973520eae22a5744bde435790
caicai250 <1551316691@qq.com>
2024-01-09 18:00:49 +0800

    初始化

---
+++ qslib-boot/qslib-security/src/test/java/com/cct/qslib/security/QslibSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cct.qslib.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QslibSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ qslib-boot/qslib/src/main/java/com/cct/qslib/sys/entity/TSysDict.java
@@ -0,0 +1,10 @@
+package com.cct.qslib.sys.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "T_SYS_DICT")
+public class TSysDict {
+
+}
